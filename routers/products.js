102142b8ba4631c2e689757a477a8f4d1cede01c
Gabriel Vullioud <gabriel.vullioud@gmail.com>
2022-11-03 22:46:21 -0300

    initial commit

---
+++ routers/products.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { Router } = express;
+const MongoProducts = require("../containers/mongoContainer");
+const products = new MongoProducts();
+const router = Router();
+
+router.get("/", async (req, res) => {
+  prod = await products.getProduct();
+  try {
+    res.send(prod);
+  } catch (error) {
+    res.send([]);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  prod = await products.getProductbyId(id);
+  try {
+    res.status(200).send(prod);
+  } catch (error) {
+    res.send([]);
+  }
+});
+
+const admin = (req, res, next) => {
+  //if(req.query.admin=="true"){
+    next()
+  //}else{
+    //res.send('no tiene permisos para acceder a la ruta')
+  //}
+}
+
+router.post("/", admin, async (req, res) => {
+    await products.createProduct(req.body);
+    const prods = await products.getProduct()
+    res.status(201).send(prods);
+}); 
+
+router.put("/:id", admin, async (req, res) => {
+    const {id} = req.params
+    await products.updateProduct(id, req.body);
+    const prod = await products.getProductbyId(id)
+    res.status(201).send(prod);
+});
+
+router.delete("/:id", admin, async (req, res) => {
+    const { id } = req.params;
+    await products.deleteProduct(id);
+    prod = await products.getProduct();
+    res.status(201).send(prod);
+});
+
+module.exports = router;
